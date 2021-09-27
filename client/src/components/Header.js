720eebc7cb0d70d2f7044de4f7f67986939f4162
importAnt <ken14250@gmail.com>
2021-09-27 17:16:28 +0800

    SOUL golden ticket NFT - BAYC Project

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="bg-black p-6 justify-center items-center flex">
+      <img
+        src="https://cdn.shopify.com/s/files/1/0276/0176/4435/files/soul-nft-logo.png?v=1630383389"
+        alt="SOUL NFT logo"
+      />
+    </div>
+  );
+}
+
+export default Header;
